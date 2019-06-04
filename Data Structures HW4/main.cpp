3944f801d52817a8c1dd1701df94755ea9b4d257
Ezzat Qupty <ezzat.qupty12345@gmail.com>
2019-06-04 12:36:58 +0300

    Initial Commit

---
+++ Data Structures HW4/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Data Structures HW4
+//
+//  Created by Ezzat Qupty on 04/06/2019.
+//  Copyright © 2019 Ezzat Qupty. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+	return 0;
+}
